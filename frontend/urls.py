7eb655d45c9b8f5ec7b6eea758069c7c0d0f096e
Alex Marandon <alex.marandon@makina-corpus.com>
2013-11-07 14:38:53 +0100

    Use namespaced URLs

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
-    '',
-    url(r'^$', 'frontend.views.main.home', name='home'),
-    url(r'^sources/create', 'frontend.views.sources.create',
-        name='create_source'),
-    url(r'^sources', 'frontend.views.sources.list',
-        name='list_sources'),
+    'frontend.views',
+    url(r'^$', 'main.home', name='home'),
+    url(r'^sources/create', 'sources.create', name='create'),
+    url(r'^sources', 'sources.list', name='list'),
 )
