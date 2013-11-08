11549ab3f17a9c820c5c1e9e2b5ebf52b656668d
Alex Marandon <alex.marandon@makina-corpus.com>
2013-11-08 16:07:28 +0100

    Merge date and time inputs

--- frontend/tests/test_events.py
+++ frontend/tests/test_events.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from django.conf import settings
 
 from .support import PatchMixin
-from frontend.views.events import merge_datetime
 
 
 class TestEvents(PatchMixin, TestCase):
@@ -29,27 +28,21 @@ class TestEvents(PatchMixin, TestCase):
         self.login()
         user_data = {
             'title': u'Un événement',
-            'start_date': '2012-01-01',
-            'start_time': '09:00',
-            'end_date': '2012-01-02',
-            'end_time': '18:00',
+            'start_time': '2012-01-01T09:00',
+            'end_time': '2012-01-02T18:00',
         }
 
         response = self.client.post('/events/create', user_data, follow=True)
 
-        api_data = dict(user_data)
-        merge_datetime(api_data)
-        self.assert_post_to_api(api_data)
+        self.assert_post_to_api(user_data)
         self.assertContains(response, 'alert-success')
 
     def test_create_invalid_event(self):
         self.login()
         invalid_data = {
             'title': u'Événement',
-            'start_date': '*** invalid date ***',
-            'start_time': '09:00',
-            'end_date': '2012-01-02',
-            'end_time': '18:00',
+            'start_time': '*** invalid datetime ***',
+            'end_time': '2012-01-02T18:00',
         }
         response_mock = self.requests_mock.post.return_value
         response_mock.json.return_value = {
@@ -57,8 +50,8 @@ class TestEvents(PatchMixin, TestCase):
             "errors": [
                 {
                     "location": "body",
-                    "name": "events.0.start_date",
-                    "description": "date is invalid"
+                    "name": "events.0.start_time",
+                    "description": "datetime is invalid"
                 },
             ]
         }
@@ -67,16 +60,14 @@ class TestEvents(PatchMixin, TestCase):
                                     follow=True)
         self.assertEqual(response.status_code, 200)
 
-        api_data = dict(invalid_data)
-        merge_datetime(api_data)
-        self.assert_post_to_api(api_data)
+        self.assert_post_to_api(invalid_data)
         self.assertContains(response, 'alert-error')
-        self.assertContains(response, u'date is invalid', count=1)
+        self.assertContains(response, u'datetime is invalid', count=1)
         self.assertContains(
             response, u'value="Événement"',
             msg_prefix="input should be pre-filled with previous input")
         self.assertContains(
-            response, u'value="*** invalid date ***"',
+            response, u'value="*** invalid datetime ***"',
             msg_prefix="input should be pre-filled with previous input")
 
     def test_event_list(self):
--- frontend/views/events.py
+++ frontend/views/events.py
@@ -7,18 +7,6 @@ from frontend.views.base import APIForm
 from frontend.api_client import APIClient
 
 
-def merge_datetime(data):
-    for prefix in ('start', 'end'):
-        time_key = prefix + '_time'
-        date_key = prefix + '_date'
-        data[time_key] = 'T'.join([
-            data.get(date_key, ''),
-            data.get(time_key, ''),
-        ])
-        if date_key in data:
-            del data[date_key]
-
-
 class Form(APIForm):
 
     template_name = 'event_form.html'
@@ -27,9 +15,6 @@ class Form(APIForm):
     success_message = u"L'événement a été enregistré avec succès"
     error_message = u"L'événement n'a pas pu être enregistré"
     resource_name_plural = 'events'
-
-    def prepare_api_input(self, data):
-        merge_datetime(data)
 
 
 @login_required
