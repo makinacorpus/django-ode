3b50823dd79f950b95b2648c247ee3a62a91eeba
Alex Marandon <alex.marandon@makina-corpus.com>
2013-11-13 18:07:33 +0100

    Wire up logout view

--- frontend/tests/test_basic.py
+++ frontend/tests/test_basic.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
 
+from accounts.models import User
+
 
 class SimpleTest(TestCase):
 
     def test_home(self):
         response = self.client.get('/')
         self.assertContains(response, '<!DOCTYPE html>')
-        self.assertContains(response, 'homepage')
+        self.assertContains(response, 'Connexion')
+        self.assertContains(response, '/accounts/login/')
+        self.assertContains(response, 'Inscription')
+        self.assertContains(response, '/accounts/signup/')
+
+    def test_home_authenticated(self):
+        User.objects.create_user(username='bob', password='foobar')
+        self.client.login(username='bob', password='foobar')
+        response = self.client.get('/')
+        self.assertContains(response, 'Déconnexion')
+        self.assertContains(response, '/accounts/logout/')
