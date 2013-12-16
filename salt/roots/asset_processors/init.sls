0f7a8cb56c0f88775aa97aa4785159ce3b55e3ae
Alex Marandon <alex.marandon@makina-corpus.com>
2013-12-16 17:45:13 +0100

    Speed up nodejs install

--- salt/roots/asset_processors/init.sls
+++ salt/roots/asset_processors/init.sls
@@ -3,9 +3,14 @@ nodejs:
     - source: salt://scripts/install_nodejs.sh
     - user: root
     - shell: /bin/bash
-    - unless: "test -e /usr/local/bin/node"
+    - unless: "test -e /usr/bin/node"
 
 less:
   cmd.run:
     - name: npm -g install less
-    - unless: "test -e /usr/local/bin/lessc"
+    - unless: "test -e /usr/bin/lessc"
+
+less:
+  cmd.run:
+    - name: npm -g install grunt-cli
+    - unless: "test -e /usr/bin/grunt"
--- salt/roots/scripts/install_nodejs.sh
+++ salt/roots/scripts/install_nodejs.sh
@@ -2,11 +2,7 @@
 
 # Install Node.js
 apt-get install python2.7 # Node build script requires Python 2
-NODE_VERSION=0.10.22
-wget -q -c http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.tar.gz
-tar xzf node-v${NODE_VERSION}.tar.gz
-cd node-v${NODE_VERSION}
-PYTHON=/usr/bin/python ./configure
-make
-make install
-
+NODE_VERSION=0.10.23
+ARCH=linux-x64
+wget -q -c http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-${ARCH}.tar.gz
+tar xz --strip-components=1 -C /usr -f node-v${NODE_VERSION}-${ARCH}.tar.gz
