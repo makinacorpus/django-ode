0d3baa2617edb815a7b2e7cf8a1c1bc2df4c1d0b
Alex Marandon <alex.marandon@makina-corpus.com>
2013-12-17 18:06:52 +0100

    Revert "Make local settings optional"
    This reverts commit 6483eda64607f0dacefb949056a01698f7d6f051.
    
    Conflicts:
    	manage.py

--- manage.py
+++ manage.py
@@ -5,9 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                           "django_ode.settings.local")
+
     from django.core.management import execute_from_command_line
-    try:
-        execute_from_command_line(sys.argv)
-    except ImportError:
-        os.environ["DJANGO_SETTINGS_MODULE"] = "django_ode.settings.base"
-        execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
