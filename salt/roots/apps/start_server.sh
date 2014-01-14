f3390b953ed12a2c6c9e8edadb8434ea401d50a3
Gagaro <gagaro42@gmail.com>
2014-01-14 11:36:46 +0100

    fix: use --waiting to quit circus

--- salt/roots/apps/start_server.sh
+++ salt/roots/apps/start_server.sh
@@ -4,6 +4,5 @@ ENV_DIR={{ env_dir }}
 PROJECT_DIR={{ project_dir }}
 . ${ENV_DIR}/bin/activate
 cd ${PROJECT_DIR}
-${ENV_DIR}/bin/circusctl quit || echo No circusd running
-sleep 3
+${ENV_DIR}/bin/circusctl quit --waiting || echo No circusd running
 ${ENV_DIR}/bin/circusd --daemon ${PROJECT_DIR}/circus.ini
