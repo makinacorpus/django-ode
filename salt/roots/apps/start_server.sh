5254d96d5595fb8793295f03fe5efbdf2c9a9da8
Gagaro <gagaro42@gmail.com>
2014-01-16 16:51:54 +0100

    fix: add log message in start_server.sh

--- salt/roots/apps/start_server.sh
+++ salt/roots/apps/start_server.sh
@@ -6,9 +6,11 @@ PROJECT_DIR={{ project_dir }}
 cd ${PROJECT_DIR}
 
 #${ENV_DIR}/bin/circusctl quit --waiting || echo No circusd running
-if ${ENV_DIR}/bin/circusctl --endpoint tcp://127.0.0.1:{{ circus_port }} status
+if ${ENV_DIR}/bin/circusctl --endpoint tcp://127.0.0.1:{{ circus_port }} status 2&> /dev/null
 then
+    echo "circusd already running. Restarting it."
     ${ENV_DIR}/bin/circusctl --endpoint tcp://127.0.0.1:{{ circus_port }} restart
 else
+    echo "circusd not running. Starting it."
     ${ENV_DIR}/bin/circusd --daemon ${PROJECT_DIR}/circus.ini
 fi
