94cac90ce1fc116914893030a72bfafdba972ff2
Gagaro <gagaro42@gmail.com>
2014-01-16 16:07:21 +0100

    fix: start_server.sh to test if circus already running

--- salt/roots/apps/start_server.sh
+++ salt/roots/apps/start_server.sh
@@ -6,4 +6,9 @@ PROJECT_DIR={{ project_dir }}
 cd ${PROJECT_DIR}
 
 #${ENV_DIR}/bin/circusctl quit --waiting || echo No circusd running
-${ENV_DIR}/bin/circusd --daemon ${PROJECT_DIR}/circus.ini || ${ENV_DIR}/bin/circusctl --endpoint tcp://127.0.0.1:{{ circus_port }} restart
+if ${ENV_DIR}/bin/circusctl --endpoint tcp://127.0.0.1:{{ circus_port }} status
+then
+    ${ENV_DIR}/bin/circusctl --endpoint tcp://127.0.0.1:{{ circus_port }} restart
+else
+    ${ENV_DIR}/bin/circusd --daemon ${PROJECT_DIR}/circus.ini
+fi
