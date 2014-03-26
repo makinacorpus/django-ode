ab6310872700c3f6c56b98166091aadcfca9401b
Natsu- <romain.garrigues.cs@gmail.com>
2014-03-26 17:01:12 +0100

    Adding logs if active logic triggers some errors...

--- frontend/templatetags/active.py
+++ frontend/templatetags/active.py
@@ -1,19 +1,29 @@
+
+import logging
+
 from django import template
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, NoReverseMatch
 
 register = template.Library()
 
 
+logger = logging.getLogger('frontend')
+
+
 @register.simple_tag(takes_context=True)
 def active(context, url):
-    request = context['request']
 
-    if url.startswith('/'):
-        path = url
-    else:
-        path = reverse_lazy(url)
+    try:
+        request = context['request']
 
-    if request.path == path:
-        return 'active'
+        if url.startswith('/'):
+            path = url
+        else:
+            path = reverse_lazy(url)
+
+        if request.path == path:
+            return 'active'
+    except NoReverseMatch as e:
+        logger.error(str(e))
 
     return ''
